5c9f2e6139c08beadb3a1f7e1b95017437a740df
MessiDaGod <MessiDaGod@users.noreply.github.com>
2021-11-16 13:22:20 -0800

    Initial commit

---
+++ store/Links.js
@@ -0,0 +1,9 @@
+export default [
+  { title: "Home", text: "Home" },
+  { title: "Bio", text: "Bio" },
+  { title: "Gallery", text: "Gallery" },
+  { title: "Form", text: "Form" },
+  { title: "Blog", text: "Blog" },
+  { title: "Pizza", text: "Pizza!" },
+  { title: "Order", text: "Order Pizza" }
+];
